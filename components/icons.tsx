fcb640b3ae68fabe931eba82a62597f16bf09968
30210200 <vraj.dudhat@ucalgary.ca>
2025-02-22 18:49:44 -0700

    added additional UI components for the landing page, and rectified minor bugs

---
+++ components/icons.tsx
@@ -0,0 +1,25 @@
+import { Moon, Sun, Laptop, type LightbulbIcon as LucideProps, ChromeIcon as Google } from "lucide-react"
+
+export const Icons = {
+  sun: Sun,
+  moon: Moon,
+  laptop: Laptop,
+  logo: (props: LucideProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M16 12h-4V8" />
+      <path d="M12 16l-4-4" />
+    </svg>
+  ),
+  google: Google,
+}
+
